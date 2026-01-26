d3acfa4fac1593397e5754c23799aacaabafc570
jana-trpkovska <107814974+jana-trpkovska@users.noreply.github.com>
2026-01-26 12:37:03 +0100

    Fine-tune cnn by adding stacked convolutional layers

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -1,10 +1,8 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Embedding, Conv1D, Dense, Dropout, Concatenate, Flatten
+from tensorflow.keras.layers import Input, Embedding, Conv1D, GlobalMaxPooling1D, Dense, Dropout, Concatenate, Flatten
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import Precision, Recall
-
-from src.models.kmax_pooling_layer_cnn import KMaxPooling
 
 
 def create_cnn_model(
@@ -15,7 +13,6 @@ def create_cnn_model(
         num_filters=128,
         kernel_sizes=(3, 4, 5),
         dropout_rate=0.5,
-        k_max=3
 ):
     inputs = Input(shape=(max_len,), name="input_ids")
 
@@ -29,14 +26,25 @@ def create_cnn_model(
 
     conv_blocks = []
     for k in kernel_sizes:
-        conv = Conv1D(
+        conv1 = Conv1D(
             filters=num_filters,
             kernel_size=k,
             activation="relu",
+            padding="same",
             kernel_regularizer=l2(1e-4),
-            name=f"conv_{k}",
+            name=f"conv1_{k}",
         )(embedding)
-        pooled = KMaxPooling(k=k_max, name=f"kmax_pool_{k}")(conv)
+
+        conv2 = Conv1D(
+            filters=num_filters * 2,
+            kernel_size=k,
+            activation="relu",
+            padding="same",
+            kernel_regularizer=l2(1e-4),
+            name=f"conv2_{k}",
+        )(conv1)
+
+        pooled = GlobalMaxPooling1D(name=f"pool_{k}")(conv2)
         conv_blocks.append(pooled)
 
     x = Concatenate(name="concat")(conv_blocks)
